fce209fe03b1fa8a10b918b3f55b6d61e34e6068
Rohan Siddhu <rohansiddhu21@gmail.com>
2021-11-29 16:57:49 +0530

    added reset button, a messagebox(displayed when puzzle is completed); reorganised: updated Application and GameFrame, created PuzzleBoard class

--- puzzle.py
+++ puzzle.py
@@ -1,13 +1,18 @@
 # puzzle.py: This file contains the starting point of the game.
 
 
+#################
 # Import Modules
+#################
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 
+#########
 # Global
+#########
 
 TileOrderTuple = (
     0, 3, 14, 6,
@@ -16,115 +21,192 @@ TileOrderTuple = (
     11, 8, 4, 15
 )
 
-TileOrderList = list(TileOrderTuple)
 
-
+####################
 # Application class
+####################
 
 class Application(tk.Tk):
     '''Main application class.'''
     def __init__(self, title = 'Tk'):
+        '''Application constructor'''
         super().__init__()
         self.title(title)
 
-        self.gameboard = GameFrame(self)
-        self.gameboard.show()
-        self.bind('<Key>', self.gameboard.key_press)
-        self.gameboard.grid(row = 0, column = 0)
+        # GameFrame
+        self.gameframe = GameFrame(self, 'img/download1.png')
+        self.gameframe.grid(row = 0, column = 0)
 
-        self.button = ttk.Button(self, text = "Sort", command = self.on_sort)
-        self.button.grid(row = 0, column = 1)
-    
-    def on_sort(self):
-        TileOrderList.sort()
-        self.gameboard.show()
+        self.bind('<Key>', self.key_callback)       # Bind the key_callback to the root application.
 
+    def key_callback(self, event):
+        '''Application key callback'''
+        self.gameframe.key_callback(event)
 
-# Game Frame
+        # Check if the puzzle is complete
+        if self.gameframe.puzzleboard.is_sorted():
+            # If puzzle is complete then ask for restart.
+            answer = messagebox.askretrycancel('Game Over!',
+                'Your completed in ' + str(self.gameframe.puzzleboard.moves) + " moves.\n Do you want to retry?")
+            if answer == True:
+                self.gameframe.reset()
+            else:
+                self.destroy()
+
+###################
+# Game Frame Class
+###################
 
 class GameFrame(ttk.Frame):
-    '''Game Board Frame'''
-    def __init__(self, parent):
-        super().__init__(parent, border = 20)
-        self.parent = parent
-        self.img = tk.PhotoImage(file = 'img/download1.png')
-        self.emptyTile = 15
-        self.tiles = []
+    '''A ttk Frame for Play Screen'''
+    def __init__(self, parent, imgPath = ''):
+        '''GameFrame constructor'''
+        super().__init__(parent, border = 10)
 
-        # row 1
-        self.tiles.append(Tile(self, self.img, x = 0, y = 0, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -150, y = 0, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -300, y = 0, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -450, y = 0, width = 150, height = 150))
-        # row 2
-        self.tiles.append(Tile(self, self.img, x = 0, y = -150, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -150, y = -150, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -300, y = -150, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -450, y = -150, width = 150, height = 150))
-        # row 3
-        self.tiles.append(Tile(self, self.img, x = 0, y = -300, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -150, y = -300, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -300, y = -300, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -450, y = -300, width = 150, height = 150))
-        # row 4
-        self.tiles.append(Tile(self, self.img, x = 0, y = -450, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -150, y = -450, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -300, y = -450, width = 150, height = 150))
-        self.tiles.append(Tile(self, self.img, x = -450, y = -450, width = 150, height = 150))
+        # Puzzle Board
+        self.puzzleboard  = PuzzleBoard(self, imgPath, order = TileOrderTuple)
+        self.puzzleboard.show()
+        self.puzzleboard.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+        # Info Frame
+        self.infoFrame = ttk.Frame(self)
+
+        # Info Frame -> Preview Frame
+        self.previewFrame = ttk.LabelFrame(self.infoFrame, text = 'Preview')
+
+        self.previewImg = tk.PhotoImage(file = 'img/download1.png')
+        self.previewImg = self.previewImg.subsample(4, 4)
+        self.previewTile = Tile(self.previewFrame, img = self.previewImg, x = 0, y = 0, width = 150, height = 150)
+        self.previewTile.grid(row = 0, column = 0, padx = 5, pady = 5, sticky = (tk.N + tk.S + tk.W + tk.E))
+
+        self.previewFrame.grid(row = 0, column = 0, columnspan = 2, sticky = (tk.N))
+
+        # Info Frame -> Moves Label
+        self.movesLabel = ttk.Label(self.infoFrame, text = "Moves:")
+        self.movesLabel.grid(row = 1, column = 0, pady = 20)
+
+        # Info Frame -> Moves Canvas (display number of moves)
+        self.movesCanvas = tk.Canvas(self.infoFrame, width = 40, height = 20)
+        self.movesCanvas_moves = self.movesCanvas.create_text(5, 5, anchor = tk.NW)
+        self.movesCanvas.itemconfig(self.movesCanvas_moves, text = str(self.puzzleboard.moves))
+        self.movesCanvas.grid(row = 1, column = 1, pady = 20, sticky = (tk.W + tk.N + tk.S))
+
+        self.infoFrame.grid(row = 0, column = 1, padx = 10, pady = 10, sticky = (tk.N))
+
+        # Reset Button
+        self.resetButton = ttk.Button(self, text = "Reset", command = lambda: self.reset())
+        self.resetButton.grid(row = 1, column = 0, columnspan = 2, pady = 5)
 
         self.columnconfigure(0, weight = 1)
     
+    def reset(self):
+        '''Reset the game frame to its initial state'''
+        self.puzzleboard.reset()
+        self.movesCanvas.itemconfig(self.movesCanvas_moves, text = str(self.puzzleboard.moves))
+    
+    def key_callback(self, event):
+        '''GameFrame key callback'''
+        self.puzzleboard.key_callback(event)
+        self.movesCanvas.itemconfig(self.movesCanvas_moves, text = str(self.puzzleboard.moves))
+
+
+###############
+# Puzzle Board
+###############
+
+class PuzzleBoard(ttk.Frame):
+    '''A ttk Frame to hold the puzzle tiles together to make a puzzle bord'''
+    def __init__(self, parent, imagePath = '', subSample = None, gridSize = 4, boardSize = 600, order = None):
+        '''PuzzleBoard constructor'''
+        super().__init__(parent)
+
+        self.tileSize = boardSize / gridSize
+        self.moves = 0
+        self.order = order or ()
+        self.orderList = list(self.order)
+        self.gridSize = gridSize
+        self.lastTile = (gridSize ** 2) - 1
+        self.emptyTile = self.lastTile
+
+        self.img = tk.PhotoImage(file = imagePath)
+        if subSample: self.img = self.img.subsample(subSample, subSample)
+        self.tiles = []
+
+        # creating tiles
+        for i in range(gridSize):
+            for j in range(gridSize):
+                self.tiles.append(Tile(self, self.img, x = - (j * self.tileSize), y = - (i * self.tileSize),
+                    width = self.tileSize, height = self.tileSize))        
+
     def show(self):
-        for i in range(16):
-            self.tiles[TileOrderList[i]].grid(row = i // 4, column = i % 4)
-        self.tiles[15].grid_forget()
+        '''Display the puzzle board'''
+        for i in range(self.gridSize ** 2):
+            self.tiles[self.orderList[i]].grid(row = i // self.gridSize, column = i % self.gridSize)
+        self.tiles[self.lastTile].grid_forget()
     
     def clear(self):
+        '''Clear the board'''
         for tile in self.tiles:
             tile.grid_forget()
     
+    def reset(self):
+        '''Reset the puzzle board to its initial state'''
+        self.orderList = list(self.order)
+        self.moves = 0
+        self.emptyTile = self.lastTile
+        self.show()
+    
     def is_sorted(self):
-        for i in range(15):
-            if (TileOrderList[i] > TileOrderList[i + 1]):
+        '''Return True if board is sorted and False if not'''
+        for i in range((self.gridSize ** 2) - 1):
+            if (self.orderList[i] > self.orderList[i + 1]):
                 return False
         else:
             return True
     
-    def key_press(self, event):
+    def key_callback(self, event):
+        '''PuzzleBoard key callback'''
         key = event.keysym
         if key == 'Right':
-            if self.emptyTile % 4:
-                TileOrderList[self.emptyTile], TileOrderList[self.emptyTile - 1] = TileOrderList[self.emptyTile - 1], TileOrderList[self.emptyTile]
+            if self.emptyTile % self.gridSize:
+                self.orderList[self.emptyTile], self.orderList[self.emptyTile - 1] = self.orderList[self.emptyTile - 1], self.orderList[self.emptyTile]
                 self.emptyTile -= 1
+                self.moves += 1
         elif key == 'Left':
-            if self.emptyTile % 4 != 3:
-                TileOrderList[self.emptyTile], TileOrderList[self.emptyTile + 1] = TileOrderList[self.emptyTile + 1], TileOrderList[self.emptyTile]
+            if self.emptyTile % self.gridSize != self.gridSize - 1:
+                self.orderList[self.emptyTile], self.orderList[self.emptyTile + 1] = self.orderList[self.emptyTile + 1], self.orderList[self.emptyTile]
                 self.emptyTile += 1
+                self.moves += 1
         elif key == 'Down':
-            if self.emptyTile // 4:
-                TileOrderList[self.emptyTile], TileOrderList[self.emptyTile - 4] = TileOrderList[self.emptyTile - 4], TileOrderList[self.emptyTile]
-                self.emptyTile -= 4
+            if self.emptyTile // self.gridSize:
+                self.orderList[self.emptyTile], self.orderList[self.emptyTile - self.gridSize] = self.orderList[self.emptyTile - self.gridSize], self.orderList[self.emptyTile]
+                self.emptyTile -= self.gridSize
+                self.moves += 1
         elif key == 'Up':
-            if self.emptyTile // 4 != 3:
-                TileOrderList[self.emptyTile], TileOrderList[self.emptyTile + 4] = TileOrderList[self.emptyTile + 4], TileOrderList[self.emptyTile]
-                self.emptyTile += 4
+            if self.emptyTile // self.gridSize != self.gridSize - 1:
+                self.orderList[self.emptyTile], self.orderList[self.emptyTile + self.gridSize] = self.orderList[self.emptyTile + self.gridSize], self.orderList[self.emptyTile]
+                self.emptyTile += self.gridSize
+                self.moves += 1
         self.show()
-        
-        if self.is_sorted():
-            self.parent.destroy()
 
 
+###############
 # Puzzle Tiles
+###############
 
 class Tile(tk.Canvas):
-    '''Canvas to hold the puzzle tile'''
-    def __init__(self, parent, img, x = 0, y = 0, *args, **kwargs):
-        super().__init__(parent, highlightthickness = 0, *args, **kwargs)
-        self.create_image(x, y, anchor = tk.NW, image = img)
+    '''A tk Canvas to hold the puzzle tile'''
+    def __init__(self, parent, img, x = 0, y = 0, thickness = 0, img_args = None, *args, **kwargs):
+        '''Tile constructor'''
+        super().__init__(parent, highlightthickness = thickness, *args, **kwargs)
+        img_args = img_args or {}
+        self.create_image(x, y, anchor = tk.NW, image = img, **img_args)
         self.columnconfigure(0, weight = 1)
 
 
+#################
 # Starting point
+#################
 
 if __name__ == '__main__':
     app = Application('Puzzle!')
